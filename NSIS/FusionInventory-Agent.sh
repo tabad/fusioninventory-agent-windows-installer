8f8f7c42c226c27d3f161d70c5fd3b6832e6fee0
Tomás Abad <tabadgp@gmail.com>
2012-08-22 18:06:29 +0200

    Initial import

---
+++ NSIS/FusionInventory-Agent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+declare -r makensis=$(type -P makensis)
+
+## Build x86 installer
+echo Please wait. Compilling x86 installer...
+${makensis} -V3 -O./FusionInventory-Agent_MakeNSIS-Output-x86.txt -DDINSTALLER_PLATFORM_ARCHITECTURE=x86 ./FusionInventory-Agent.nsi
+echo Done!
+
+## Build x86 installer
+echo Please wait. Compilling x64 installer...
+${makensis} -V3 -O./FusionInventory-Agent_MakeNSIS-Output-x64.txt -DDINSTALLER_PLATFORM_ARCHITECTURE=x64 ./FusionInventory-Agent.nsi
+echo Done!
