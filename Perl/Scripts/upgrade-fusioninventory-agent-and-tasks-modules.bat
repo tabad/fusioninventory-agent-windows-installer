fc779295e5a997079914c5bf99024122a903c84a
Tomás Abad <tabadgp@gmail.com>
2012-10-04 19:50:38 +0200

    Set GNU GPL License 2.

--- Perl/Scripts/upgrade-fusioninventory-agent-and-tasks-modules.bat
+++ Perl/Scripts/upgrade-fusioninventory-agent-and-tasks-modules.bat
@@ -9,14 +9,21 @@
 ::
 :: This file is part of FusionInventory project.
 ::
-:: FusionInventory is free software: you can [...]
+:: FusionInventory Agent Installer for Microsoft Windows is free software;
+:: you can redistribute it and/or modify it under the terms of the GNU
+:: General Public License as published by the Free Software Foundation;
+:: either version 2 of the License, or (at your option) any later version.
 ::
-:: FusionInventory is distributed in the hope that it will be useful,
-:: but WITHOUT ANY WARRANTY; without even the implied warranty of
-:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-:: GNU Affero General Public License for more details.
 ::
-:: You should have received a copy of [...]
+:: FusionInventory Agent Installer for Microsoft Windows is distributed in
+:: the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+:: the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+:: PURPOSE. See the GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program; if not, write to the Free Software Foundation,
+:: Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA,
+:: or see <http://www.gnu.org/licenses/>.
 ::
 :: ------------------------------------------------------------------------
 ::
@@ -24,7 +31,8 @@
 :: @file      .\Perl\Scripts\upgrade-fusioninventory-agent-and-tasks-modules.bat    
 :: @author    Tomas Abad
 :: @copyright Copyright (c) 2010-2012 FusionInventory Team
-:: @license   [...]
+:: @license   GNU GPL version 2 or (at your option) any later version
+::            http://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
 :: @link      http://www.fusioninventory.org/
 :: @link      http://forge.fusioninventory.org/projects/fusioninventory-agent
 :: @since     2012
