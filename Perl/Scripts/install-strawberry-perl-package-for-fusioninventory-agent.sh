b47024eccdcd1897305e3064f082515493b93ceb
Tomás Abad <tabadgp@gmail.com>
2012-11-04 12:08:48 +0100

    Cosmetic.

--- Perl/Scripts/install-strawberry-perl-package-for-fusioninventory-agent.sh
+++ Perl/Scripts/install-strawberry-perl-package-for-fusioninventory-agent.sh
@@ -99,7 +99,7 @@ fi
 
 # Download ${strawberry_pepfia_url}/${strawberry_pepfia_file}
 echo "Installing Strawberry Perl ${strawberry_release} (${strawberry_version}-32/64bits)"
-echo -n " Portable Edition Package for FusionInventory-Agent (BuildId ${strawberry_pepfia_build_id})."
+echo -n " Portable Edition Package for FusionInventory-Agent (${strawberry_pepfia_branch}-${strawberry_pepfia_build_id})."
 ${curl} --silent --location --max-redirs 6 --output "/tmp/${strawberry_pepfia_file}" \
    "${strawberry_pepfia_url}/${strawberry_pepfia_file}" > /dev/null 2>&1
 
